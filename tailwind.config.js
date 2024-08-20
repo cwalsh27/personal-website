33003581827ca66702b783d12b9a362a2250c1df
Coleman <cowalsh@udel.edu>
2024-08-20 00:16:59 -0400

    added new fonts and colors for redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmSerif: ["DM Serif Text", "serif"],
+        fatFace: ["Abril Fatface", "serif"],
+        jakarta: ["Plus Jakarta Sans", "sans-serif"]
+      },
+      colors: {
+        forestGreen: '#2B4141',
+        backgroundGrey: '#dbdfe6',
+        clayBlueGrey: '#cfe2f3ff'
+      },
+    },
   },
   plugins: [],
 }
