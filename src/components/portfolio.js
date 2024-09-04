e7e86d015123ccb2cbce36e3206d96ff24eb46b7
Coleman <cowalsh@udel.edu>
2024-09-04 10:06:15 -0400

    began populating portfolio cards, and experimented wtih solving white screen bounce issue. No dice. Could be as simple as disabling side scroll on the html/body

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,10 +7,10 @@ function Portfolio() {
     return (
         
         <div class="scrolling-wrapper" className="w-full h-[80vh] bg-background flex flex-nowrap overflow-x-auto items-center">
-            <PortfolioCard title="Project Title" desc="THis is hte desc of the project"></PortfolioCard>
-            <PortfolioCard title="Project Title" desc="THis is hte desc of the project"></PortfolioCard>
-            <PortfolioCard title="Project Title" desc="THis is hte desc of the project"></PortfolioCard>
-            <PortfolioCard title="Project Title" desc="THis is hte desc of the project"></PortfolioCard>
+            <PortfolioCard title="JobNav" desc="Worked on a team of four software engineers to develop a website with two career quizzes, one with multiple choice answer and one that implements ChatGPT API to produce customized outputs for novel question responses."></PortfolioCard>
+            <PortfolioCard title="Bigram/Trigram Language Model" desc="Built algorithm that trains on a corpus of common sentences and then produces novel ones based on next word probabilities. Further adapted that algorithm into a rudimentary predictive text algorithm for a hypothetical email client."></PortfolioCard>
+            <PortfolioCard title="SLAM Lab Data Reconciliation" desc="Authored scripts to automate existing manual data reconciliation process, reducing its duration by 70%"></PortfolioCard>
+            <PortfolioCard title="Seafloor Rasterizer" desc="Worked on small team to develop rasterization scripts for sonar imagery of seafloor, preparing the data for AI segmentation tasks with ResNet-18 backbone."></PortfolioCard>
             <PortfolioCard title="Project Title" desc="THis is hte desc of the project"></PortfolioCard>
         </div>  
         
