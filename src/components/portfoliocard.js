3d2a1651d2e092838ebf4d3731d18612176a88ae
Coleman <cowalsh@udel.edu>
2024-09-04 16:49:38 -0400

    addd hover motions for portfolio cards

--- src/components/portfoliocard.js
+++ src/components/portfoliocard.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function PortfolioCard (props) {
     return (
-        <div className="h-[75%] w-[27%] ml-9 mr-9 rounded-2xl shadow-2xl bg-gradient-to-br from-white to-slate-200 flex justify-between flex-col" style={{flex: "0 0 auto"}}>
+        <motion.div  
+        whileHover={{scale: 1.1}}
+        className="h-[75%] w-[27%] mx-12 rounded-2xl shadow-2xl bg-gradient-to-br from-white to-slate-200 flex justify-between flex-col hover:scale-105" style={{flex: "0 0 auto"}}>
             <div>
                 <h2 className="text-5xl font-dmSerif mt-8 opacity-100">{props.title}</h2>
                 <p className="text-xl font-dmSerif mt-8 mx-7 opacity-100">{props.desc}</p>
@@ -10,7 +13,7 @@ function PortfolioCard (props) {
             <div className="mb-8">
                 <p className="text-xl font-dmSerif">{props.plurality}{props.langs}</p>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
