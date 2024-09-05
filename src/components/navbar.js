fe02ab7bf81e5cb656463ec5d38fd2562593b106
Coleman <cowalsh@udel.edu>
2024-09-05 13:54:27 -0400

    added hover color change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,10 +14,10 @@ function Navbar() {
             <img src="../imgs/signature.png" className='h-full w-full'></img>
            </div>
            <div className='flex justify-around items-center mr-[5vw] mt-[1vh]'>
-            <a className='ml-[3vw] text-lg text-backgroundGrey'>RESUME</a>
-            <a className='ml-[3vw] text-lg text-slate-400'>RESEARCH</a>
-            <a className='ml-[3vw] text-lg text-slate-400'>PROJECTS</a>
-            <a className='ml-[3vw] text-lg text-slate-400'>CONTACT</a>
+            <motion.a href="" whileHover={{color: "#dbdfe6"}} className='ml-[3vw] text-lg text-backgroundGrey'>RESUME</motion.a>
+            <motion.a href="" whileHover={{color: "#dbdfe6"}} className='ml-[3vw] text-lg text-navbarDefaultColor'>RESEARCH</motion.a>
+            <motion.a href="" whileHover={{color: "#dbdfe6"}} className='ml-[3vw] text-lg text-slate-400'>PROJECTS</motion.a>
+            <motion.a href="" whileHover={{color: "#dbdfe6"}} className='ml-[3vw] text-lg text-slate-400'>CONTACT</motion.a>
            </div>
         </motion.div>
         
